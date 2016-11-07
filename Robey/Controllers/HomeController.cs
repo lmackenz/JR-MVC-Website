922f84e5f98af5aee8c74160723f07f215483f7a
lmackenz <coder@straypennies.com>
2016-11-06 19:26:11 -0500

    entering a change

--- Robey/Controllers/HomeController.cs
+++ Robey/Controllers/HomeController.cs
@@ -26,5 +26,7 @@ namespace Robey.Controllers
 
             return View();
         }
+
+     
     }
 }
---
+++ Robey/Models/EventsModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Robey.Models
+{
+    public class EventsModels
+    {
+
+        public int EventID { get; set; }
+
+        public string EventName { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Room { get; set; }
+
+
+
+
+    }
+}
